0580976f2c6010ae4383c05cbb1aa9c4a0c06e19
Nathan Miller <nami2227@colorado.edu>
2024-09-10 12:44:46 -0600

    FEAT: Added ability for the number of plastic parameters to change

--- src/cpp/tests/test_tardigrade_micromorphic_elasto_plasticity.cpp
+++ src/cpp/tests/test_tardigrade_micromorphic_elasto_plasticity.cpp
@@ -9,6 +9,9 @@
 #define BOOST_TEST_MODULE test_tardigrade_micromorphic_elasto_plasticity
 #include <boost/test/included/unit_test.hpp>
 
+#define DEFAULT_TEST_TOLERANCE 1e-6
+#define CHECK_PER_ELEMENT boost::test_tools::per_element( )
+
 typedef tardigradeMicromorphicTools::constantType constantType;
 typedef tardigradeMicromorphicTools::constantVector constantVector;
 typedef tardigradeMicromorphicTools::constantMatrix constantMatrix;
@@ -11305,5 +11308,243 @@ BOOST_AUTO_TEST_CASE( testEvaluateHydraModel){
     BOOST_CHECK( tardigradeVectorTools::fuzzyEquals( dFpdGradPhi,       ADD_JACOBIANS[ 2 ] ) );
     BOOST_CHECK( tardigradeVectorTools::fuzzyEquals( dChipdGradPhi,     ADD_JACOBIANS[ 5 ] ) );
     BOOST_CHECK( tardigradeVectorTools::fuzzyEquals( dGradChipdGradPhi, ADD_JACOBIANS[ 8 ] ) );
+
+}
+
+BOOST_AUTO_TEST_CASE( testParameterExtraction){
+    /*!
+     * Test the evaluation of the constitutive model.
+     *
+     */
+
+    //Initialize the time
+    std::vector< double > time = { 10., 2.5 };
+
+    double temperature = 293.15;
+
+    double previousTemperature = 293.15;
+
+    //Initialize the material parameters
+    std::vector< double > fparams = { 4, 2.4e2, 1.5e1, 1e-2, 0.12, //Macro hardening parameters
+                                      4, 1.4e2, 2.0e1, 2e-2, 0.13, //Micro hardening parameters
+                                      4, 2.0e0, 2.7e1, 3e-2, 0.14, //Micro gradient hardening parameters
+                                      2, 0.56, 0.2,                //Macro flow parameters
+                                      2, 0.15,-0.2,                //Micro flow parameters
+                                      2, 0.82, 0.1,                //Micro gradient flow parameters
+                                      2, 0.70, 0.3,                //Macro yield parameters
+                                      2, 0.40,-0.3,                //Micro yield parameters
+                                      2, 0.52, 0.4,                //Micro gradient yield parameters
+                                      2, 696.47, 65.84,            //A stiffness tensor parameters
+                                      5, -7.69, -51.92, 38.61, -27.31, 5.13,  //B stiffness tensor parameters
+                                      11, 1.85, -0.19, -1.08, -1.57, 2.29, -0.61, 5.97, -2.02, 2.38, -0.32, -3.25, //C stiffness tensor parameters
+                                      2, -51.92, 5.13,             //D stiffness tensor parameters
+                                      0.4, 0.3, 0.35, 1e-8, 1e-8   //Integration parameters
+                                    };
+
+    //Initialize the gradient of the macro displacement
+//    double current_grad_u[ 3 ][ 3 ] = { { -1.83182277, -0.66558173,  0.23458272 },
+//                                        { -0.56632666, -0.21399259,  0.16367238 },
+//                                        { -0.29129789, -0.22367825, -2.0632945  } };
+//
+//    double previous_grad_u[ 3 ][ 3 ] = { { -1.89906429,  0.20890208, -0.39814132 },
+//                                         {  0.31303067, -1.23910631, -0.93837662 },
+//                                         { -0.32571524, -0.95306342, -0.93025257 } };
+
+    double current_grad_u[ 3 ][ 3 ] = { {0.200, 0.100, 0.000 },
+                                        {0.100, 0.001, 0.000 },
+                                        {0.000, 0.000, 0.000 } };
+
+    double previous_grad_u[ 3 ][ 3 ] = { {0, 0, 0},
+                                         {0, 0, 0},
+                                         {0, 0, 0} };
+    //Initialize the micro displacement
+//    double current_phi[ 9 ] = { 0.84729289,  0.40617104,  0.59534561,  
+//                                0.44195587,  0.34121966, -0.79098944, 
+//                               -0.43965428,  0.88466225,  0.1684519 };
+//
+//    double previous_phi[ 9 ] = { -0.99935855, -0.21425717,  0.0668254 ,
+//                                 -0.11111872, -0.07416114, -1.01048108,
+//                                  0.1804018 , -1.01116291,  0.03248007 };
+
+    double current_phi[ 9 ] = { 0.100, 0.000, 0.000,
+                                0.000, 0.000, 0.000,
+                                0.000, 0.000, 0.000 };
+
+    double previous_phi[ 9 ] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    //Initialize the gradient of the micro displacement
+    double current_grad_phi[ 9 ][ 3 ] = { {  0.13890017, -0.3598602 , -0.08048856 },
+                                          { -0.18572739,  0.06847269,  0.22931628 },
+                                          { -0.01829735, -0.48731265, -0.25277529 },
+                                          {  0.26626212,  0.4844646 , -0.31965177 },
+                                          {  0.49197846,  0.19051656, -0.0365349  },
+                                          { -0.06607774, -0.33526875, -0.15803078 },
+                                          {  0.09738707, -0.49482218, -0.39584868 },
+                                          { -0.45599864,  0.08585038, -0.09432794 },
+                                          {  0.23055539,  0.07564162,  0.24051469 } };
+
+//    double previous_grad_phi[ 9 ][ 3 ] = { { -0.47850242,  0.36472234,  0.37071411 },
+//                                           {  0.00294417,  0.34480654, -0.34450988 },
+//                                           {  0.21056511, -0.28113967, -0.45726839 },
+//                                           { -0.26431286, -0.09985721,  0.47322301 },
+//                                           { -0.18156887, -0.32226199, -0.37295847 },
+//                                           {  0.15062371,  0.09439471,  0.09167948 },
+//                                           { -0.46869859,  0.018301  ,  0.45013866 },
+//                                           { -0.15455446,  0.40552715, -0.4216042  },
+//                                           { -0.38930237,  0.10974753, -0.31188239 } };
+
+//    double current_grad_phi[ 9 ][ 3 ] = { {0, 0, 0},
+//                                          {0, 0, 0},
+//                                          {0, 0, 0},
+//                                          {0, 0, 0},
+//                                          {0, 0, 0},
+//                                          {0, 0, 0},
+//                                          {0, 0, 0},
+//                                          {0, 0, 0},
+//                                          {0, 0, 0} };
+
+    double previous_grad_phi[ 9 ][ 3 ] = { {0, 0, 0},
+                                           {0, 0, 0},
+                                           {0, 0, 0},
+                                           {0, 0, 0},
+                                           {0, 0, 0},
+                                           {0, 0, 0},
+                                           {0, 0, 0},
+                                           {0, 0, 0},
+                                           {0, 0, 0} };
+                                           
+
+    //Initialize the state variable vector
+    std::vector< double > SDVSDefault( 55, 0 );
+
+    //Initialize the additional degree of freedom vectors
+    std::vector< double > current_ADD_DOF;
+    std::vector< std::vector< double > > current_ADD_grad_DOF;
+
+    std::vector< double > previous_ADD_DOF;
+    std::vector< std::vector< double > > previous_ADD_grad_DOF;
+
+    //Initialize the stress measures
+    std::vector< double > PK2_result( 9, 0 );
+
+    std::vector< double > SIGMA_result( 9, 0 );
+
+    std::vector< double > M_result( 27, 0 );
+
+    //Initialize the additional terms vector
+    std::vector< std::vector< double > > ADD_TERMS;
+
+    //Initialize the output message string
+    std::string output_message;
+
+#ifdef DEBUG_MODE
+    tardigradeSolverTools::homotopyMap homotopyDEBUG;
+#endif
+
+    tardigradeSolverTools::floatVector PK2_answer = { 1.72374955e+02,  1.53586446e+01, -9.18594711e-01,  1.34613772e+01,
+        1.42758448e+02, -2.14353350e-02, -1.76155689e+00,  1.77580288e+00,
+        1.41003910e+02 };
+
+    tardigradeSolverTools::floatVector SIGMA_answer = { 176.85221932,  15.84248153,  -2.83823499,  15.84248153,
+       144.5183467 ,   1.85778345,  -2.83823499,   1.85778345,
+       141.99307014 };
+
+    tardigradeSolverTools::floatVector M_answer = { 0.60016978, -0.51045338,  0.61980289,  3.23507063,  1.16925208,
+        1.20665651,  0.56034359, -2.52042105,  1.62648849, -2.61882314,
+       -0.61143851, -1.02227749,  0.67046998,  0.49701267, -0.23999964,
+       -2.77670511,  0.75636495,  1.71897722, -0.49808019,  2.62569695,
+       -0.76002078,  1.23462488, -0.00650376, -2.25591243, -0.73016414,
+        0.74380723,  0.90861263 };
+
+    tardigradeSolverTools::floatVector SDVS_answer = {  7.52790780e-03,  5.06936872e-03, -3.00562092e-04,  5.05251313e-03,
+                                                       -2.63763651e-03,  2.87928507e-04, -3.72441969e-04,  2.25230220e-04,
+                                                       -1.58168041e-03,  7.52714199e-03,  4.60154810e-03, -3.01710599e-04,
+                                                        5.56787320e-03, -2.63687070e-03,  2.58160226e-04, -3.65069825e-04,
+                                                        2.58160226e-04, -1.58168041e-03,  3.83196420e-02, -2.45387739e-02,
+                                                       -8.94156844e-03,  1.04125027e-02, -5.47145710e-04,  2.21268303e-02,
+                                                        1.85810005e-02, -1.14241984e-02, -7.62221572e-03,  4.54399478e-02,
+                                                        3.21567189e-02,  2.10892064e-02,  1.18343147e-02,  1.63154826e-02,
+                                                        1.01776270e-03,  1.47855827e-02,  7.10955065e-03, -2.40560608e-02,
+                                                        9.30082927e-03, -3.52878321e-02,  2.18504370e-02, -2.49577455e-02,
+                                                        9.18791192e-03,  2.13410715e-02,  1.69866062e-02,  1.77613753e-02,
+                                                        2.03937074e-02, -5.42245900e-23,  5.17942700e-03,  3.02931048e-02,
+                                                        1.58261418e-02,  1.60309023e-02, -1.79683900e-22,  2.11178609e-02,
+                                                        8.23638353e-02,  4.30296627e-02,  4.35864383e-02 };
+
+//    tardigradeSolverTools::floatVector PK2_answer = { 1.72381583e+02,  1.53490623e+01, -9.17869152e-01,  1.34514475e+01,
+//        1.42784323e+02, -2.26855750e-02, -1.76082294e+00,  1.77464785e+00,
+//        1.41021846e+02 };
+//
+//    tardigradeSolverTools::floatVector SIGMA_answer = { 176.85984866,  15.83223893,  -2.83753359,  15.83223893,
+//       144.5458737 ,   1.85655805,  -2.83753359,   1.85655805,
+//       142.01240974 };
+//
+//    tardigradeSolverTools::floatVector M_answer = { 0.60016027, -0.51048754,  0.61984844,  3.23507227,  1.16942361,
+//        1.20662324,  0.56038461, -2.52062122,  1.6263198 , -2.61891654,
+//       -0.61168873, -1.02203605,  0.67065387,  0.4970073 , -0.23999741,
+//       -2.7769288 ,  0.75667767,  1.71898866, -0.49803026,  2.62584805,
+//       -0.75966908,  1.23459271, -0.00667791, -2.25598899, -0.73030698,
+//        0.7438434 ,  0.90872187 };
+//
+//    tardigradeSolverTools::floatVector SDVS_answer = {  7.53391712e-03,  5.08826292e-03, -3.02187662e-04,  5.07135003e-03,
+//                                                       -2.66948991e-03,  2.90841417e-04, -3.74157919e-04,  2.27820300e-04,
+//                                                       -1.59391881e-03,  7.53314870e-03,  4.61870100e-03, -3.03219139e-04,
+//                                                        5.58862820e-03, -2.66872150e-03,  2.60913496e-04, -3.66895165e-04,
+//                                                        2.60913496e-04, -1.59391881e-03,  3.83381848e-02, -2.45330931e-02,
+//                                                       -8.95538204e-03,  1.04165665e-02, -5.32012910e-04,  2.21443208e-02,
+//                                                        1.85774208e-02, -1.14594874e-02, -7.63911022e-03,  4.54127235e-02,
+//                                                        3.21129124e-02,  2.11070183e-02,  1.18241526e-02,  1.63088860e-02,
+//                                                        1.03455560e-03,  1.47830982e-02,  7.09499615e-03, -2.40626243e-02,
+//                                                        9.31063821e-03, -3.52663045e-02,  2.18795269e-02, -2.49636009e-02,
+//                                                        9.20397912e-03,  2.13550248e-02,  1.69839898e-02,  1.77600107e-02,
+//                                                        2.03926694e-02,  3.09063946e-16,  5.21174400e-03,  3.02958646e-02,
+//                                                        1.58295896e-02,  1.60303582e-02,  1.03245982e-15,  2.12496399e-02,
+//                                                        8.23712094e-02,  4.30389579e-02,  4.35848261e-02 };
+
+    std::vector< double > SDVS = SDVSDefault;
+
+    tardigradeSolverTools::floatVector currentDeformationGradient, currentMicroDeformation, currentGradientMicroDeformation;
+
+    tardigradeSolverTools::floatVector previousDeformationGradient, previousMicroDeformation, previousGradientMicroDeformation;
+
+    tardigradeMicromorphicElastoPlasticity::assembleFundamentalDeformationMeasures( current_grad_u, current_phi, current_grad_phi,
+                                                                                    currentDeformationGradient, currentMicroDeformation,
+                                                                                    currentGradientMicroDeformation );
+
+    tardigradeMicromorphicElastoPlasticity::assembleFundamentalDeformationMeasures( previous_grad_u, previous_phi, previous_grad_phi,
+                                                                                    previousDeformationGradient, previousMicroDeformation,
+                                                                                    previousGradientMicroDeformation );
+
+    tardigradeMicromorphicElastoPlasticity::hydraMicromorphicElastoPlasticity hydra( time[ 0 ], time[ 1 ],
+                                                                                     temperature,                     previousTemperature,
+                                                                                     currentDeformationGradient,      previousDeformationGradient,
+                                                                                     currentMicroDeformation,         previousMicroDeformation,
+                                                                                     currentGradientMicroDeformation, previousGradientMicroDeformation,
+                                                                                     { }, { },
+                                                                                     SDVS, fparams, 2, 10, 3, 45, 1e-9, 1e-9, 20, 10, 1e-4, true, 0 );
+
+    tardigradeMicromorphicElastoPlasticity::hydraMicromorphicElastoPlasticityOptimization hydraOpt( time[ 0 ], time[ 1 ],
+                                                                                                    temperature,                     previousTemperature,
+                                                                                                    currentDeformationGradient,      previousDeformationGradient,
+                                                                                                    currentMicroDeformation,         previousMicroDeformation,
+                                                                                                    currentGradientMicroDeformation, previousGradientMicroDeformation,
+                                                                                                    { }, { },
+                                                                                                    SDVS, fparams, 2, 10, 3, 45, 1e-9, 1e-9, 20, 10, 1e-4, true, 0 );
+
+    tardigradeSolverTools::floatVector plasticParameters( fparams.begin( ), fparams.begin( ) + 33 );
+
+    tardigradeSolverTools::floatVector elasticParameters( fparams.begin( ) + 33, fparams.begin( ) + 57 );
+
+    BOOST_TEST( hydra.getNumPlasticParameters( ) == 33 );
+
+    BOOST_TEST( hydra.getPlasticParameters( ) == plasticParameters, CHECK_PER_ELEMENT );
+
+    BOOST_TEST( hydra.getElasticParameters( ) == elasticParameters, CHECK_PER_ELEMENT );
+
+    BOOST_TEST( hydraOpt.getNumPlasticParameters( ) == 33 );
+
+    BOOST_TEST( hydraOpt.getPlasticParameters( ) == plasticParameters, CHECK_PER_ELEMENT );
+
+    BOOST_TEST( hydraOpt.getElasticParameters( ) == elasticParameters, CHECK_PER_ELEMENT );
 
 }
